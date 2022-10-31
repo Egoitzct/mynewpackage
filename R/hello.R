9439811063bdb391068d7932585165f21d5feb17
Egoitzct <egoitzcarral@gmail.com>
2022-10-31 15:35:08 +0100

    a

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' My Hello World Function
+#'
+#' @param x The name of the person to say hi to
+#'
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("Egoitz")
+hello <- function(x) {
+  print(paste0("Hello ", x, ", this is the world!"))
+}
